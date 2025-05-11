50678af11f22424ef2034e0787a6696a9c4f59da
= <ramzan.webdeveloper@gmail.com>
2025-05-11 16:05:07 +0530

    Coversion of data types in javascript

--- test.js
+++ test.js
@@ -6,4 +6,19 @@ console.log(typeof cat);
 let animal = Number(cat);
 console.log(typeof animal);
 console.log(animal);
+// false => 0
+// true => 1
+// 10 = 10
+// '10' = 10
+// '10anb' = NaN;
+// undefined = Nan
 
+
+let a = 'vhx';
+let b = Boolean(a);
+console.log(typeof b);
+console.log(b);
+
+//1 => true
+// 0 => false
+// 'ramzan' => true
