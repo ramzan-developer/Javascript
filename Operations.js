42299427089ae1bbbc557fbdde95ccba9b92041a
= <ramzan.webdeveloper@gmail.com>
2025-05-11 23:28:19 +0530

    final changes to Operations in javascript

--- Operations.js
+++ Operations.js
@@ -3,3 +3,15 @@ console.log("1" + 2);   // 12
 console.log(1 + 2 + "3");   // 33
 console.log(1 + "2" + "3"); // 123
 
+let intial = 10;
+let final = 10;
+
+// console.log(intial++); // 10
+// console.log(intial); // 11
+
+// console.log(++final); // 11
+// console.log(final++); // 11
+// console.log(final); // 12
+// console.log(final++); // 12 
+// console.log(final); // 13
+
